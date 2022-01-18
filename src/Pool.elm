2c579716dbca80e2206a7aae09f13bc2b2c27e9d
shadow <sd@sd.com>
2022-01-18 21:32:29 +0800

    rollup

--- src/Pool.elm
+++ src/Pool.elm
@@ -4,10 +4,13 @@ module Pool exposing (..)
 -- and pool every 100ms
 
 import Browser
-import Html exposing (Html, text, pre)
+import Html exposing (Html, text, pre, button, div)
+import Html.Events exposing (onClick)
 import Http
 import Bytes exposing (Bytes)
-
+import Task
+import Time
+import Url.Builder 
 
 -- MAIN
 
@@ -25,51 +28,41 @@ main =
 -- MODEL
 
 
-type Model
-  = Failure
-  | Loading
-  | Success String
+type alias Model = 
+    { connection : String
+    , lastMsg : String
+    }
 
+initial_model : Model
+initial_model = 
+    Model "" ""
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Loading
+  ( initial_model 
   , Cmd.none 
   )
-
-connect_cmd = 
-  Http.get
-      { url = "http://127.0.0.1:8081/connect?host=www.google.com&port=80"
-      , expect = Http.expectString ConnectRes 
-      }
-
-read_conn = 
-    let token = ""
-    in Http.get
-      { url = "http://127.0.0.1:8081/pool?token=" ++ token 
-      , expect = Http.expectString ReadRes 
-      }
-
-write_conn data = 
-    let token = ""
-        body = Http.stringBody "application/json" data 
-    in Http.post
-      { url = "http://127.0.0.1:8081/push?token=" ++ token 
-      , body = body 
-      , expect = Http.expectString WriteRes
-      }
-
 
 -- UPDATE
 
 
 type Msg
   = ConnectRes (Result Http.Error String)
+  | DisconnectRes (Result Http.Error String)
   | ReadRes (Result Http.Error String)
   | WriteRes (Result Http.Error String)
+  | Tick Time.Posix
+  | Connect
+  | Disconnect
 
 
+on_connect : Model -> (Model, Cmd Msg)
+on_connect model =
+    (model, write_conn model.connection "GET / HTTP/1.1\r\n\r\n")
 
+on_read : Model -> String -> (Model, Cmd Msg)
+on_read model fullText =
+    ({ model | lastMsg = fullText}, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -77,13 +70,42 @@ update msg model =
     ConnectRes result ->
       case result of
         Ok fullText ->
-          (Success fullText, Cmd.none)
+          on_connect { model | connection = fullText }
 
         Err _ ->
-          (Failure, Cmd.none)
+          (model, Cmd.none)
+
+    ReadRes result ->
+      case result of
+        Ok fullText ->
+          on_read model fullText 
+
+        Err _ ->
+          (model, Cmd.none)
+
+
+    WriteRes result ->
+      case result of
+        Ok fullText ->
+          (model, Cmd.none)
+
+        Err _ ->
+          (model, Cmd.none)
+
+
+    Tick newTime ->
+      ( model 
+      , read_conn model.connection 
+      )
+
+    Connect ->
+        ( model, connect "www.bing.com" 80)
+
+    Disconnect ->
+        ( model, disconnect model.connection )
 
     _ ->
-          (Failure, Cmd.none)
+          (model, Cmd.none)
 
 
 
@@ -92,7 +114,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Time.every 100 Tick
 
 
 
@@ -101,12 +123,52 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  case model of
-    Failure ->
-      text "I was unable to load your book."
+    case model.connection of
+        "" ->
+          div []
+            [ button [ onClick Connect ] [ text "connect" ]
+            ]
+        _ ->
+          div []
+            [ button [ onClick Disconnect ] [ text "disconnect" ]
+            , div [] [ text model.connection ]
+            , div [] [ text model.lastMsg ]
+            ]
 
-    Loading ->
-      text "Loading..."
+makeReq action params = 
+     Url.Builder.crossOrigin "http://127.0.0.1:8091" [action] params
 
-    Success fullText ->
-      pre [] [ text fullText ]
+connect : String -> Int -> Cmd Msg
+connect host dport = 
+    let uri = makeReq "connect" [
+          Url.Builder.string "host" host
+          , Url.Builder.int "port" dport
+          ]
+    in Http.get
+           { url = uri 
+           , expect = Http.expectString ConnectRes 
+           }
+
+disconnect token = 
+  Http.get
+      { url = "http://127.0.0.1:8081/disconnect?token=" ++ token
+      , expect = Http.expectString DisconnectRes 
+      }
+
+
+read_conn token = 
+   Http.get
+      { url = "http://127.0.0.1:8081/pool?token=" ++ token 
+      , expect = Http.expectString ReadRes 
+      }
+
+write_conn token data = 
+    let 
+        body = Http.stringBody "application/json" data 
+    in Http.post
+      { url = "http://127.0.0.1:8081/push?token=" ++ token 
+      , body = body 
+      , expect = Http.expectString WriteRes
+      }
+
+
